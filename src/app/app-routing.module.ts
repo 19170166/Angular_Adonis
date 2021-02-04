4ecf97e3b0f26b234246776af14b75c443386a3d
angel zapata <angelzapata582@gmail.com>
2021-02-04 11:13:48 -0600

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PrincipalComponent} from '../app/Componentes/principal/principal.component';
+import {LoginComponent} from '../app/Componentes/login/login.component';
+import {RegistroComponent} from '../app/Componentes/registro/registro.component';
+import {InsertarComponent} from '../app/Componentes/insertar/insertar.component';
+import {PageNotFoundComponent} from '../app/Componentes/page-not-found/page-not-found.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component:LoginComponent},
+  {path: 'registro', component:RegistroComponent},
+  {path: 'insertar', component:InsertarComponent},
+  {path: '', component:PrincipalComponent},
+  {path: '**', component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Componentes/login/login.component';
+import { RegistroComponent } from './Componentes/registro/registro.component';
+import { InsertarComponent } from './Componentes/insertar/insertar.component';
+import { PrincipalComponent } from './Componentes/principal/principal.component';
+import { PageNotFoundComponent } from './Componentes/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    InsertarComponent,
+    PrincipalComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
